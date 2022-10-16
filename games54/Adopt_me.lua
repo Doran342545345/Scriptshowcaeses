95d382496710051b674c8a94ebc60f757cb7390e
Doran342545345 <105158686+Doran342545345@users.noreply.github.com>
2022-10-16 04:13:36 +0200

    Update Adopt_me.lua

--- games54/Adopt_me.lua
+++ games54/Adopt_me.lua
@@ -1,1 +1,10 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/V.G-Hub/main/V.Ghub'))()
+
+local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+   wait(1)
+   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
+
+print("Anti-AFK has been loaded")
